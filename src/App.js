65def4223e1f3000a6bceef26590d9cc94253a96
thisisaj1999 <atuljoshi94170@gmail.com>
2023-01-26 12:57:23 +0530

    Context API added with open close functionality

--- src/App.js
+++ src/App.js
@@ -23,10 +23,12 @@ import {
   Editor,
 } from './pages';
 
+import { UseStateContext } from './contexts/ContextProvider';
+
 import './App.css';
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = UseStateContext();
 
   return (
     <div>
